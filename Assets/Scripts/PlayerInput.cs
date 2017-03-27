d8b5a69236f18f2167e415dedebef7831ceb2e21
RichardRam87 <r.ram@ma-web.nl>
2017-03-27 12:04:11 +0200

    Added a basic rotate function

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is responsible for reading the input of the player
+/// Mouse and / or Keyboard
+/// </summary>
+public class PlayerInput : MonoBehaviour 
+{
+	private PlayerMovement _playerMovement;
+
+	void Awake()
+	{
+		_playerMovement = GetComponent<PlayerMovement> ();
+	}
+
+	void Update()
+	{
+		// convert pixel coords of mouse to ray.
+		// A ray is an invisible line between two points
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+
+		Debug.DrawRay (ray.origin, ray.direction * 100);
+
+		if (Physics.Raycast (ray, out hit)) 
+		{
+			_playerMovement.LookAt (hit.point);	
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The PlayerMovement class moves and rotates the player
+/// </summary>
+public class PlayerMovement : MonoBehaviour 
+{
+	public void LookAt(Vector3 point)
+	{
+		transform.LookAt (point);
+	}
+}
