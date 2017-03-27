dafe891d0a1f1515a253d2ae807d98f93068dc6f
RichardRam87 <r.ram@ma-web.nl>
2017-03-27 13:56:07 +0200

    simple player rotating

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class handles all mouse and keyboard input
+/// provided by the user.
+/// </summary>
+public class PlayerInput : MonoBehaviour 
+{
+	private PlayerMovement _playerMovement; 
+
+	void Awake()
+	{
+		_playerMovement = GetComponent<PlayerMovement> ();
+	}
+
+	void Update()
+	{
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit))
+		{
+			print (hit.collider.name);
+			_playerMovement.LookAt(hit.point);
+			Debug.DrawRay (ray.origin, ray.direction * 100);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Moves and rotates the player.
+/// </summary>
+public class PlayerMovement : MonoBehaviour 
+{
+	public void LookAt(Vector3 point)
+	{
+		transform.LookAt (point);
+	}
+}
